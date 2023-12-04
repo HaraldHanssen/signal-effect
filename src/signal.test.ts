c130f02b1214fb91fe3a4e9121b7f62f331d9036
euphbit <4030188+HaraldHanssen@users.noreply.github.com>
2023-12-04 21:17:08 +0100

    Added effect

--- src/signal.test.ts
+++ src/signal.test.ts
@@ -1,4 +1,5 @@
-import { derived, readonly, signal } from "./signal";
+import { derived, effect, readonly, signal } from "./signal";
+
 test("get signal value", () => {
     const s = signal(42);
     expect(s()).toBe(42);
@@ -8,6 +9,7 @@ test("set signal value", () => {
     s(43);
     expect(s()).toBe(43);
 });
+
 test("get readonly value", () => {
     const s = signal(42);
     const r = readonly(s);
@@ -15,12 +17,14 @@ test("get readonly value", () => {
     s(43);
     expect(r()).toBe(43);
 });
+
 test("get derived value", () => {
     let calculated = 0;
     const s = signal(42);
     const d = derived(s, (x) => {
+        expect(x).toBeDefined();
         calculated++;
-        return 2*x;
+        return 2 * x;
     });
     expect(calculated).toBe(0);
     expect(d()).toBe(84);
@@ -36,8 +40,10 @@ test("get derived value from N signals", () => {
     let calculated = 0;
     const s = signal(42);
     const t = signal(4);
-
-    const d = derived([s,t], ([x, y]) => {
+    const d = derived([s, t], ([x, y]) => {
+        expect(x).toBeDefined();
+        expect(y).toBeDefined();
+        expect(x).not.toEqual(y);
         calculated++;
         return x + y;
     });
@@ -49,21 +55,92 @@ test("get derived value from N signals", () => {
     s(41);
     expect(d()).toBe(44);
     expect(calculated).toBe(3);
+    expect(d()).toBe(44);
+    expect(calculated).toBe(3);
 });
 test("derived value is only affected by dependent signals", () => {
     let calculated = 0;
     const s = signal(42);
     const t = signal(4);
-    const d = derived(s, (x) => {
+    const u = signal(2);
+    const d = derived(s, u, (x, y) => {
+        expect(x).toBeDefined();
+        expect(y).toBeDefined();
+        expect(x).not.toEqual(y);
         calculated++;
-        return 2*x;
+        return 2 * x * y;
     });
-    expect(d()).toBe(84);
+    expect(d()).toBe(168);
     expect(calculated).toBe(1);
     t(3);
-    expect(d()).toBe(84);
-    expect(calculated).toBe(1);
+    expect(d()).toBe(168);
+    expect(calculated).toBe(1); // not called
     s(43);
-    expect(d()).toBe(86);
+    expect(d()).toBe(172);
     expect(calculated).toBe(2);
+    u(1)
+    expect(d()).toBe(86);
+    expect(calculated).toBe(3);
+});
+
+test("effect of signal", () => {
+    let acted = 0;
+    const s = signal(42);
+    const e = effect(s, (x) => {
+        expect(x).toBeDefined();
+        acted++;
+    });
+    expect(acted).toBe(0);
+    e();
+    expect(acted).toBe(1);
+    s(43);
+    expect(acted).toBe(1);
+    e();
+    expect(acted).toBe(2);
+    e();
+    expect(acted).toBe(2);
+});
+test("effect of N signals", () => {
+    let acted = 0;
+    const s = signal(42);
+    const t = signal(4);
+    const e = effect([s, t], ([x, y]) => {
+        expect(x).toBeDefined();
+        expect(y).toBeDefined();
+        expect(x).not.toEqual(y);
+        acted++;
+    });
+    e();
+    expect(acted).toBe(1);
+    t(3);
+    e();
+    expect(acted).toBe(2);
+    s(41);
+    e();
+    expect(acted).toBe(3);
+    e();
+    expect(acted).toBe(3);
+});
+test("derived value is only affected by dependent signals", () => {
+    let acted = 0;
+    const s = signal(42);
+    const t = signal(4);
+    const u = signal(2);
+    const e = effect(s, u, (x, y) => {
+        expect(x).toBeDefined();
+        expect(y).toBeDefined();
+        expect(x).not.toEqual(y);
+        acted++;
+    });
+    e();
+    expect(acted).toBe(1);
+    t(3);
+    e();
+    expect(acted).toBe(1); // not called
+    s(43);
+    e();
+    expect(acted).toBe(2);
+    u(1);
+    e();
+    expect(acted).toBe(3);
 });
--- src/signal.ts
+++ src/signal.ts
@@ -1,11 +1,16 @@
 /** A readable signal supports reading the current value */
 export interface ReadableSignal<T> {
-    (): T,
+    (): T
 }
 
 /** A writable signal supports writing the next value */
 export interface WritableSignal<T> extends ReadableSignal<T> {
     (next: T): void
+}
+
+/** An effect performs an action depending on one or more sources if they have changed */
+export interface Effect {
+    (): void
 }
 
 /** A readable signal type */
@@ -15,7 +20,7 @@ export type ReadableSignalType = ReadableSignal<any>;
 export type ReadableSignalValue<T> = T extends ReadableSignal<infer U> ? U : never;
 
 /** Array of readable signals */
-export type ReadableSignals = [ReadableSignalType, ...Array<ReadableSignalType>] | Array<ReadableSignalType>;
+export type ReadableSignalTypes = [ReadableSignalType, ...Array<ReadableSignalType>] | Array<ReadableSignalType>;
 
 /** Array of values from readable signals */
 export type ReadableSignalValues<T> = { [K in keyof T]: T[K] extends ReadableSignal<infer U> ? U : never };
@@ -31,21 +36,53 @@ export function readonly<T>(signal: WritableSignal<T>): ReadableSignal<T> {
 }
 
 /** Create a derived/calculated signal from one or more sources */
-export function derived<P extends ReadableSignalType, T>(r: P, calculate: (r: ReadableSignalValue<P>) => T): ReadableSignal<T>;
+export function derived<P extends ReadableSignalType, T>(r: P, calc: (r: ReadableSignalValue<P>) => T): ReadableSignal<T>;
 export function derived<P1 extends ReadableSignalType, P2 extends ReadableSignalType, T>
-    (r1: P1, r2: P2, calculate: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>) => T): ReadableSignal<T>;
+    (r1: P1, r2: P2, calc: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>) => T): ReadableSignal<T>;
 export function derived<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType, T>
-    (r1: P1, r2: P2, r3: P3, calculate: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>) => T): ReadableSignal<T>;
+    (r1: P1, r2: P2, r3: P3, calc: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>) => T): ReadableSignal<T>;
 export function derived<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType, P4 extends ReadableSignalType, T>
-    (r1: P1, r2: P2, r3: P3, r4: P4, calculate: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>) => T): ReadableSignal<T>;
+    (r1: P1, r2: P2, r3: P3, r4: P4, calc: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>) => T): ReadableSignal<T>;
 export function derived<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType, P4 extends ReadableSignalType, P5 extends ReadableSignalType, T>
-    (r1: P1, r2: P2, r3: P3, r4: P4, r5: P5, calculate: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>, r5: ReadableSignalValue<P5>) => T): ReadableSignal<T>;
-export function derived<P extends ReadableSignals, T>(sources: P, calculate: (values: ReadableSignalValues<P>) => T): ReadableSignal<T>
+    (r1: P1, r2: P2, r3: P3, r4: P4, r5: P5, calc: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>, r5: ReadableSignalValue<P5>) => T): ReadableSignal<T>;
+export function derived<P extends ReadableSignalTypes, T>(sources: P, calc: (values: ReadableSignalValues<P>) => T): ReadableSignal<T>
 export function derived(...args: any[]): any {
     if (args.length < 2) throw Error("Expected at least 2 parameters!");
-    const s = (args.length == 2 && Array.isArray(args[0]) ? args[0] : args.slice(0, -1)).map(x => (x as unknown as SignalInfoRef<any>)._self);
-    const d = args.slice(-1)[0] as Calc<any>;
+    if (args.length == 2 && Array.isArray(args[0])) {
+        const s = args[0].map(x => (x as unknown as SignalInfoRef<any>)._self);
+        const d = args.slice(-1)[0] as Calc<any>;
+        return asDerived({ s, v: MIN_V, t: undefined, d });
+    }
+
+    const s = args.slice(0, -1).map(x => (x as unknown as SignalInfoRef<any>)._self);
+    const dd = args.slice(-1)[0] as ((...a:any[]) => any);
+    const d = ((a:any[]) => dd(...a)) as Calc<any>;
     return asDerived({ s, v: MIN_V, t: undefined, d });
+}
+
+/** Create an effect/action from one or more sources */
+export function effect<P extends ReadableSignalType>(r: P, act: (r: ReadableSignalValue<P>) => void): Effect;
+export function effect<P1 extends ReadableSignalType, P2 extends ReadableSignalType>
+    (r1: P1, r2: P2, act: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>) => void): Effect;
+export function effect<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType>
+    (r1: P1, r2: P2, r3: P3, act: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>) => void): Effect;
+export function effect<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType, P4 extends ReadableSignalType>
+    (r1: P1, r2: P2, r3: P3, r4: P4, act: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>) => void): Effect;
+export function effect<P1 extends ReadableSignalType, P2 extends ReadableSignalType, P3 extends ReadableSignalType, P4 extends ReadableSignalType, P5 extends ReadableSignalType>
+    (r1: P1, r2: P2, r3: P3, r4: P4, r5: P5, act: (r1: ReadableSignalValue<P1>, r2: ReadableSignalValue<P2>, r3: ReadableSignalValue<P3>, r4: ReadableSignalValue<P4>, r5: ReadableSignalValue<P5>) => void): Effect;
+export function effect<P extends ReadableSignalTypes>(sources: P, act: (values: ReadableSignalValues<P>) => void): Effect
+export function effect(...args: any[]): any {
+    if (args.length < 2) throw Error("Expected at least 2 parameters!");
+    if (args.length == 2 && Array.isArray(args[0])) {
+        const s = args[0].map(x => (x as unknown as SignalInfoRef<any>)._self);
+        const e = args.slice(-1)[0] as Act;
+        return asEffect({ s, v: MIN_V, e });
+    }
+
+    const s = args.slice(0, -1).map(x => (x as unknown as SignalInfoRef<any>)._self);
+    const ee = args.slice(-1)[0] as ((...a:any[]) => void);
+    const e = ((a:any[]) => ee(...a)) as Act;
+    return asEffect({ s, v: MIN_V, e });
 }
 
 // Internals
@@ -71,10 +108,18 @@ function maxV(values: VersionType[]): VersionType {
     return m;
 }
 
+type Node = {
+    /** The version number representing the current value or effect */
+    v: VersionType
+};
+
+type DependentNode = Node & {
+    /** The signal sources */
+    s: SignalInfo<any>[]
+};
+
 /** Signal information */
-type SignalInfo<T> = {
-    /** The version number representing the current value */
-    v: VersionType,
+type SignalInfo<T> = Node & {
     /** The current value */
     t?: T
 };
@@ -82,18 +127,25 @@ type SignalInfo<T> = {
 /** Reference field is added to the facades */
 type SignalInfoRef<T> = {
     _self: SignalInfo<T>
-}
+};
 
 /** Derived information */
-type DerivedInfo<T> = SignalInfo<T> & {
-    /** The signal sources */
-    s: SignalInfo<any>[]
+type DerivedInfo<T> = SignalInfo<T> & DependentNode & {
     /** The calculate function */
     d: Calc<T>
-}
+};
 
 /** Calculation signature */
-type Calc<T> = (...args: any[]) => T;
+type Calc<T> = (args: any[]) => T;
+
+/** Effect information */
+type EffectInfo = DependentNode & {
+    /** The action */
+    e: Act
+};
+
+/** Action signature */
+type Act = (args: any[]) => void;
 
 /** Wrap info in a writable facade */
 function asWritable<T>(info: SignalInfo<T>): WritableSignal<T> & SignalInfoRef<T> {
@@ -131,6 +183,25 @@ function asDerived<T>(info: DerivedInfo<T>): ReadableSignal<T> & SignalInfoRef<T
         }
         self.v = cv;
         return self.t!;
+    };
+    f._self = info;
+    return f;
+}
+
+
+/** Wrap info in an effect facade */
+function asEffect(info: EffectInfo): Effect {
+    const f = (): void => {
+        const self = f._self;
+        const cv = currV();
+        if (cv > self.v) {
+            //Changes has occured. Check dependencies.
+            const m = maxV(self.s.map(x => x.v));
+            if (m > self.v) {
+                self.e(self.s.map(x => x.t));
+            }
+        }
+        self.v = cv;
     };
     f._self = info;
     return f;
